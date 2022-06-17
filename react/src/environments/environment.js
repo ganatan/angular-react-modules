c8ff727a1bf897b5bf0448106241f280fdba7852
ganatan <dannyganatan@gmail.com>
2022-06-17 16:43:47 +0200

    Update react environment

--- react/src/environments/environment.js
+++ react/src/environments/environment.js
@@ -1,7 +1,7 @@
 export const environment = {
   application:
   {
-    name: 'react-bootstrap',
+    name: 'react-modules',
     version: 'React 18.2.0',
     bootstrap: 'Bootstrap 5.1.3',
     fontawesome: 'Font Awesome 6.1.1',
