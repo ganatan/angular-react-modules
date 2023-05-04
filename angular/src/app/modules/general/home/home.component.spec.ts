fabd3e4ec9fc47d314f7e88fa05babc28a7fac60
ganatan <dannyganatan@gmail.com>
2023-05-04 15:47:12 +0200

    Update Angular 16.0.0

--- angular/src/app/modules/general/home/home.component.spec.ts
+++ angular/src/app/modules/general/home/home.component.spec.ts
@@ -1,21 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeComponent } from './home.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [HomeComponent]
-    })
-      .compileComponents();
-
+    });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
